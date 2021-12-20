c6978f7efb0d10e91c1603b7de6955e2020a3d2e
Saotomefirst <jorge.appleseed@gmail.com>
2021-12-19 21:19:42 -0300

    Montagem inicial da mecânica da aplicação
    Aplicação funcional desenvolvida sem o paradigma do Ciclo de Vida. Funciona, mas está bagunçada.

--- app/src/main/java/com/saotome/vmodel/MainActivity.kt
+++ app/src/main/java/com/saotome/vmodel/MainActivity.kt
@@ -2,10 +2,60 @@ package com.saotome.vmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.saotome.vmodel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var txtContador: EditText
+    lateinit var btnDados: Button
+    lateinit var btnMostrar: Button
+
+    var contador = 0
+
+    private lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        val activityMainview = activityMainBinding.root
+        setContentView(activityMainview)
+        //setContentView(R.layout.activity_main)
+
+        inicializarDados()
+        inicializarContador()
+        inicializarCliques()
+
     }
+
+    private fun validarContador() {
+        if (contador > 5) {
+            contador = 0
+        }
+    }
+
+    private fun inicializarDados() {
+        txtContador = activityMainBinding.txtContador
+        btnDados = activityMainBinding.btnDados
+        btnMostrar = activityMainBinding.btnMostrar
+    }
+
+    private fun inicializarContador() {
+        txtContador.setText(contador.toString())
+    }
+
+    private fun inicializarCliques() {
+        btnDados.setOnClickListener {
+            contador++
+            validarContador()
+            inicializarContador()
+        }
+
+        btnMostrar.setOnClickListener {
+            Toast.makeText(this, "Valor do Contador: ${contador.toString()}", Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
